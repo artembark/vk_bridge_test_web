937a671fa5dfaaa0d4290a8b2bdfd9a36887de47
Artem Barkalov <artembark@gmail.com>
2022-04-07 17:17:51 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/vk_bridge_test_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vk_bridge_test_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:vk_bridge/vk_bridge.dart';
+
+//import 'examples_page.dart';
+
+class SimpleLogger implements Logger {
+  @override
+  void d(Object message) {
+    print('vk_bridge.d: $message');
+  }
+
+  @override
+  void e(Object message) {
+    print('vk_bridge.e: $message');
+  }
+}
+
+Future<void> main() async {
+  VKBridge.instance.setLogger(SimpleLogger());
+
+  final result = await VKBridge.instance.init();
+
+  print('VKBridge.init: $result');
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
