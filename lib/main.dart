40196380bada552ed06d155fc7121dc588f86549
Artem Barkalov <artembark@gmail.com>
2022-04-10 02:06:51 +0300

    testing refresh no future up materialApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,46 +4,41 @@ import 'package:vk_bridge/src/data/model/results/vk_web_app_get_user_info_result
 import 'package:vk_bridge_test_web/testing_page.dart';
 
 Future<void> main() async {
-  final result = await VKBridge.instance.init();
+  //final result = await VKBridge.instance.init();
 
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
-
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
             child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Приветы ', //+
-                        //(snapshot.data as VKWebAppGetUserInfoResult)
-                        //  .firstName,
-                        style: const TextStyle(fontSize: 40.0),
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const TestingPage(),
-                              ),
-                            );
-                          },
-                          child: const Text('Поиграем?'))
-                    ],
-                  )
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Приветы ', //+
+              //(snapshot.data as VKWebAppGetUserInfoResult)
+              //  .firstName,
+              style: const TextStyle(fontSize: 40.0),
             ),
-          ),
-        ),
-      );
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const TestingPage(),
+                    ),
+                  );
+                },
+                child: const Text('Поиграем?'))
+          ],
+        )),
+      ),
+    );
   }
 }
