1bcbe6d9afcefda2ac8d5bbd3134d2d1184401c6
Artem Barkalov <artembark@gmail.com>
2022-04-10 18:42:45 +0300

    vk bridge init, updateConfigStream,locationChangedStream, viewHideStream

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:vk_bridge/src/data/model/results/vk_web_app_get_user_info_result
 import 'package:vk_bridge_test_web/testing_page.dart';
 
 Future<void> main() async {
-  //final result = await VKBridge.instance.init();
+  final result = await VKBridge.instance.init();
 
-  //print('VKBridge.init: $result');
+  print('VKBridge.init: $result');
 
   runApp(MyApp());
 }
@@ -15,51 +15,74 @@ Future<String> data =
     Future.delayed(Duration(seconds: 2)).then((value) => 'Hello');
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: FutureBuilder(
-              //future: VKBridge.instance.getUserInfo(),
-              future: data,
-              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Text('Загружаем...');
-                }
-                if (snapshot.hasError) {
-                  return const Text('FutureBuilder Error');
-                }
-                if (snapshot.hasData) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Привет ',// +
-                           // (snapshot.data as VKWebAppGetUserInfoResult)
-                            //    .firstName,
-                        style: const TextStyle(fontSize: 40.0),
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const TestingPage(),
-                              ),
-                            );
-                          },
-                          child: const Text('Поиграем?'))
-                    ],
-                  );
-                }
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    VKBridge.instance.updateConfigStream;
+    VKBridge.instance.locationChangedStream;
+    VKBridge.instance.viewHideStream;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: FutureBuilder(
+            future: VKBridge.instance.getUserInfo(),
+            //future: data,
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
                 return Text('Загружаем...');
-              },
-            ),
+              }
+              if (snapshot.hasError) {
+                return const Text('FutureBuilder Error');
+              }
+              if (snapshot.hasData) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Привет ' +
+                          (snapshot.data as VKWebAppGetUserInfoResult)
+                              .firstName,
+                      style: const TextStyle(fontSize: 40.0),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const TestingPage(),
+                            ),
+                          );
+                        },
+                        child: const Text('Поиграем?'))
+                  ],
+                );
+              }
+              return Text('Загружаем...');
+            },
           ),
         ),
       ),
